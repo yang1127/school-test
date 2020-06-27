f46bf2d0f3e465a2a6b63cf29e8399f381e8b092
yang1127 <1536520643@qq.com>
2020-06-27 23:33:49 +0800

    练习自动化测试

--- 测试/062601.py
+++ 测试/062601.py
@@ -10,7 +10,8 @@ browser.find_element_by_id("kw").send_keys("selenium")
 time.sleep(3)
 browser.find_element_by_id("su").click()
 browser.quit()
-'''
+
+
 
 #coding=utf-8
 from selenium import webdriver
@@ -42,3 +43,39 @@ browser.find_element_by_id("su").click()
 time.sleep(3)
 browser.quit()
 
+
+#coding=utf-8
+from selenium import webdriver
+browser = webdriver.Firefox()
+browser.get("http://www.baidu.com")
+browser.find_element_by_link_text("hao123").click()
+browser.quit()
+
+#coding=utf-8
+from selenium import webdriver
+browser = webdriver.Firefox()
+browser.get("http://www.baidu.com")
+browser.find_element_by_link_text("hao123").click()
+browser.quit()
+
+'''
+
+#coding=utf-8
+from selenium import webdriver
+import time
+driver = webdriver.Firefox()
+driver.get("http://www.baidu.com")
+time.sleep(2)
+
+driver.find_element_by_id("kw").send_keys("test")
+time.sleep(2)
+
+driver.find_element_by_id("kw").clear()
+driver.find_element_by_id("kw").send_keys("selenium")
+time.sleep(2)
+
+#通过submit() 来操作
+driver.find_element_by_id("su").submit()
+
+time.sleep(3)
+driver.quit()
